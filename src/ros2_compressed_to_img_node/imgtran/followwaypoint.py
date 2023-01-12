771a8f9409c4e0d0400863f8e87b612eb5841a73
freshmea <freshmea@naver.com>
2023-01-12 13:36:11 +0900

    followwaypoints

--- src/ros2_compressed_to_img_node/imgtran/followwaypoint.py
+++ src/ros2_compressed_to_img_node/imgtran/followwaypoint.py
@@ -1,11 +1,12 @@
 import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import PoseStamped 
+from geometry_msgs.msg import PoseStamped
 from rclpy.action import ActionClient
 from action_msgs.msg import GoalStatus
 from nav2_msgs.action import FollowWaypoints
 # from rclpy.duration import Duration # Handles time for ROS 2
- 
+# from action_msgs.msg import GoalStatus
+
 class ClientFollowPoints(Node):
 
     def __init__(self):
@@ -33,7 +34,14 @@ class ClientFollowPoints(Node):
 
     def get_result_callback(self, future):
         result = future.result().result
-        self.get_logger().info('Result: {0}'.format(result.missed_waypoints))
+        # action_status = future.result().status
+        # if action_status == GoalStatus.STATUS_SUCCEEDED:
+        self.get_logger().info(f'Result: {result.missed_waypoints}')
+            # self.get_logger().info(f'Result: {action_status}')
+        # else :
+        #     self.get_logger().info(f'Result: action failed Result: {action_status}')
+        # self.get_logger().info(f'Result: {action_status}')
+        rclpy.shutdown()
 
     def feedback_callback(self, feedback_msg):
         feedback = feedback_msg.feedback
@@ -50,12 +58,25 @@ def main(args=None):
     rgoal.header.stamp.sec = 0
     rgoal.header.stamp.nanosec = 0
     rgoal.pose.position.z = 0.0
-    rgoal.pose.position.x = 0.2
-    rgoal.pose.position.y = 1.3
+    rgoal.pose.position.x = -3.6
+    rgoal.pose.position.y = -1.3
     rgoal.pose.orientation.w = 1.0
-    print(rgoal)
     mgoal = [rgoal]
 
     follow_points_client.send_points(mgoal)
-
     rclpy.spin(follow_points_client)
+
+    rclpy.init(args=args)
+    follow_points_client = ClientFollowPoints()
+    rgoal2 = PoseStamped()
+    rgoal2.header.frame_id = "map"
+    rgoal2.header.stamp.sec = 0
+    rgoal2.header.stamp.nanosec = 0
+    rgoal2.pose.position.z = 0.0
+    rgoal2.pose.position.x = -2.2
+    rgoal2.pose.position.y = -1.1
+    rgoal2.pose.orientation.w = 1.0
+    mgoal = [rgoal2]
+    follow_points_client.send_points(mgoal)
+    rclpy.spin(follow_points_client)
+    print('ok')
